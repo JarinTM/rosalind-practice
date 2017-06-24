a080d393490f259fa2612a9d470e0fc3cadaac65
JarinTM <jtmourosi@gmail.com>
2017-06-25 00:24:48 +0600

    My first commit
    This is a test on how to do commits in GitHub using upload ...

---
+++ highest GC.py
@@ -0,0 +1,13 @@
+#dnaseq={'Rosalind_6404':'CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCCTCCCACTAATAATTCTGAGG',
+#'Rosalind_5959':'CCATCGGTAGCGCATCCTTAGTCCAATTAAGTCCCTATCCAGGCGCTCCGCCGAAGGTCTATATCCATTTGTCAGCAGACACGC',
+#'Rosalind_0808':'CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGACTGGGAACCTGCGGGCAGTAGGTGGAAT'}
+#print dnaseq.keys()
+#print dnaseq.values()
+##gc=float(float)
+#for value in dnaseq():
+#     g
+##print(float( va.count('C')+f.count('G'))/len (f))*100
+#from Bio.SeqUtils import GC
+#print GC(dnaseq)
+f=open("pythona.txt",'r').readline().rstrip()
+print f
